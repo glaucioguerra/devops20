c14787c608e307f183adbbef45c55d1cdccaae7c
Glaucio Guerra <glaucioguerra@gmail.com>
2024-06-23 16:18:50 -0300

    Delete lab-ansible directory

--- lab-ansible/notes-app/src/main/java/academy/guerra/notes/repository/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package academy.guerra.notes.repository;
-
-import academy.guerra.notes.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
